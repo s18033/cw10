b420c843e204b8de7bf414214d9c82511210ea2e
Piotr Kwiatek <p.kwiatek@mindz.it>
2020-05-26 23:56:51 +0200

    project init

---
+++ s18033-10/Controllers/StudentsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using s18033_10.Models;
+
+namespace s18033_10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly s18033Context _context;
+
+        public StudentsController(s18033Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudent()
+        {
+            return await _context.Student.ToListAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(string id, Student student)
+        {
+            if (id != student.IndexNumber)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(string id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return student;
+        }
+
+        private bool StudentExists(string id)
+        {
+            return _context.Student.Any(e => e.IndexNumber == id);
+        }
+    }
+}
